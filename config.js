29b12fb8671ab9074cbe4c0b4dd712488a0dfb40
Hammy Havoc <hammy@splitanatom.com>
2017-12-20 13:01:21 +0000

    Initial commit.

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'mr-big-win-android',
+		wp_ws_url : 'https://www.mrbigwin.com/wp-appkit-api/mr-big-win-android',
+		wp_url : 'https://www.mrbigwin.com',
+		theme : 'q-android-1.0.6',
+		version : '1.0',
+		app_title : 'Mr Big Win',
+		app_platform : 'android',
+		gmt_offset : 1,
+		debug_mode : 'off',
+		auth_key : 'e6kcsndk6genfzhgqhmipoy7crtyfhqxwmvevkeuz1ftj8sspxp0knizjyvbbavf',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
